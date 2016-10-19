683c84e05094fe8b14e2c0294454af01af262cd4
Isaac Zafuta <isaac@zafuta.com>
2016-10-18 21:15:28 -0700

    Sets up the yarn/babel/react stack

---
+++ src/components/Homepage/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './style.css';
+
+class Homepage extends Component {
+  render() {
+    return (
+      <div className="Homepage">
+        <pre>{`Meticulous, yes. Methodical. Educated. They were these things.
+
+Nothing extreme. Like anyone, they varied. There were days of mistakes and laziness and infighting.
+And there were days, good days, when by anyone's judgment, they would have to be considered clever.
+
+They took from their surroundings what was needed, and made of it something more.`}</pre>
+      </div>
+    );
+  }
+}
+
+export default Homepage;
