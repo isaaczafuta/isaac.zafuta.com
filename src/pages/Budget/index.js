e1e533ed90b9b55e1efd947a47699f87e9b9a647
Isaac Zafuta <isaac@zafuta.com>
2017-07-29 01:11:39 -0700

    Pretty some stuff up

--- src/pages/Budget/index.js
+++ src/pages/Budget/index.js
@@ -1,3 +1,4 @@
+import classNames from 'classnames';
 import React, { Component } from 'react';
 import moment from 'moment';
 
@@ -11,13 +12,25 @@ class Budget extends Component {
     this.state = {
       amount: '',
       notes: '',
-      expenses: []
+      submitting: false,
+      notificationDismissed: false,
+      expenses: [{
+        id: 1,
+        timestamp: moment.now(),
+        amount: 65,
+        notes: 'This is some shit',
+      },{
+        id: 2,
+        timestamp: moment.now(),
+        amount: 65,
+        notes: 'This is some shit',
+      }]
     };
 
     fetch('/api/expenses')
     .then((r) => r.json())
     .then((json) => {
-      json.data.sort((a, b) => a.timestamp.localeCompare(b.timestamp));
+      json.data.sort((a, b) => b.timestamp.localeCompare(a.timestamp));
       this.setState({expenses: json.data});
     });
 
@@ -26,35 +39,76 @@ class Budget extends Component {
   render = () => {
     const numDays = moment().diff(moment("20170502", "YYYYMMDD"), 'days');
     const amounts = this.state.expenses.map((expense) => expense.amount);
-    let remainder = (5000 * numDays) - amounts.reduce(((a, b) => a + b), 0);
+    const remainder = (6400 * numDays) - amounts.reduce(((a, b) => a + b), 0);
+
+    let notification = null;
+    if (remainder < 0 && !this.state.notificationDismissed) {
+      notification = (
+        <div className="notification is-danger has-text-centered">
+          <button className="delete" onClick={e => this.setState({notificationDismissed: true})} />
+          Slow down there buddy! You're over budget by ${Number(-remainder / 100).toFixed(2)}!
+        </div>
+      );
+    } else if (!this.state.notificationDismissed) {
+      notification = (
+        <div className="notification has-text-centered">
+          <button className="delete" onClick={e => this.setState({notificationDismissed: true})} />
+          You have ${Number(remainder / 100).toFixed(2)} left to spend!
+        </div>
+      );
+    }
 
     return (
       <Page>
         <Navigation/>
-        <div className="Budget">
-          <input type="text"
-                 placeholder="amount"
-                 value={this.state.amount}
-                 onChange={(e) => this.setState({amount: e.target.value})} />
-          <input type="text"
-                 placeholder="notes"
-                 value={this.state.notes}
-                 onChange={(e) => this.setState({notes: e.target.value})} />
-          <button onClick={this.handleClick}>Submit</button>
-          <div>
-            Remaining: ${Number(remainder / 100).toFixed(2)}
+        <div className="container">
+          <h1 className="title has-text-centered">Budget</h1>
+          {notification}
+          <div className="field has-addons has-addons-centered">
+            <p className="control">
+              <input autoFocus
+                     tabIndex="1"
+                     className="input"
+                     type="text"
+                     placeholder="Amount"
+                     value={this.state.amount}
+                     onChange={(e) => this.setState({amount: e.target.value})} />
+            </p>
+            <p className="control">
+              <input className="input"
+                     tabIndex="2"
+                     type="text"
+                     placeholder="Notes"
+                     value={this.state.notes}
+                     onChange={(e) => this.setState({notes: e.target.value})} />
+            </p>
+            <p className="control">
+              <button tabIndex="3"
+                      className={classNames('button', 'is-primary', {'is-loading': this.state.submitting})}
+                      onClick={this.handleClick}>
+                Save
+              </button>
+            </p>
           </div>
-          <table>
+
+          <table className="table is-striped">
+            <thead>
+            <tr>
+              <th>Amount</th>
+              <th>Date</th>
+              <th>Notes</th>
+            </tr>
+            </thead>
             <tbody>
-              {this.state.expenses.map((expense) => {
-                return (
+            {this.state.expenses.map(expense => {
+              return (
                 <tr key={expense.id}>
-                  <td>{expense.timestamp}</td>
                   <td>${Number(expense.amount / 100).toFixed(2)}</td>
+                  <td>{moment(expense.timestamp).format('lll')}</td>
                   <td>{expense.notes}</td>
                 </tr>
-                );
-              })}
+              )
+            })}
             </tbody>
           </table>
         </div>
@@ -67,6 +121,10 @@ class Budget extends Component {
 
     formData.append('amount', this.state.amount);
     formData.append('notes', this.state.notes);
+
+    this.setState({
+      submitting:true
+    });
 
     fetch('/api/expenses', {
       method: 'put',
