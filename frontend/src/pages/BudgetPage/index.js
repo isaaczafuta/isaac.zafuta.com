5b99ab851119e616da0d1d4b875e24ebbec1dbc9
Isaac Zafuta <isaac@zafuta.com>
2018-09-09 19:06:49 -0700

    Fix build...

--- frontend/src/pages/BudgetPage/index.js
+++ frontend/src/pages/BudgetPage/index.js
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import React, { Component } from "react";
+import React from "react";
 import moment from "moment";
 import PropTypes from "prop-types";
 
--- frontend/src/pages/HomePage/index.js
+++ frontend/src/pages/HomePage/index.js
@@ -3,7 +3,6 @@ import React from 'react';
 import {Navigation} from "../../components/layout/Navigation";
 import {Page} from "../../components/layout/Page";
 
-import "./style.css";
 
 class HomePage extends React.Component {
 
