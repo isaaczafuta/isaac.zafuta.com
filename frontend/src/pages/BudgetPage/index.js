537c5a0dee87c5cd338ece78e19dc72be279ff5c
Isaac Zafuta <isaac@zafuta.com>
2018-09-24 21:28:55 -0700

    Avoid reloading

--- frontend/src/pages/BudgetPage/index.js
+++ frontend/src/pages/BudgetPage/index.js
@@ -1,6 +1,7 @@
 import classNames from "classnames";
 import React from "react";
 import moment from "moment";
+import 'moment-timezone';
 import PropTypes from "prop-types";
 
 import {CurrencyInput} from "../../components/CurrencyInput";
@@ -11,11 +12,12 @@ import {Page} from "../../components/layout/Page";
 class ExpenseEditor extends React.Component {
   static propTypes = {
     expense: PropTypes.object.isRequired,
-    onClose: PropTypes.func.isRequired,
+    onEditConfirmed: PropTypes.func.isRequired,
+    onEditCanceled: PropTypes.func.isRequired,
   };
 
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props);
 
     this.state = {
       amount: this.props.expense.amount,
@@ -24,9 +26,9 @@ class ExpenseEditor extends React.Component {
     }
   }
 
-  saveChanges = () => {
+  _saveChanges = () => {
     if (this.state.amount === this.props.expense.amount && this.state.notes === this.props.expense.notes) {
-      this.props.onClose(false);
+      this.props.onEditCanceled();
     } else {
       const formData = new FormData();
       formData.append("amount", this.state.amount);
@@ -42,7 +44,9 @@ class ExpenseEditor extends React.Component {
         method: "PATCH",
         body: formData
       }).then((r) => {
-        this.props.onClose(true);
+        // TODO: don't mutate
+        this.props.expense.amount = this.state.amount;
+        this.props.onEditConfirmed(this.props.expense);
       });
     }
   };
@@ -54,7 +58,7 @@ class ExpenseEditor extends React.Component {
         <div className="modal-card">
           <header className="modal-card-head">
             <p className="modal-card-title">Edit Expense</p>
-            <button className="delete" onClick={() => this.props.onClose(false)} />
+            <button className="delete" onClick={this.props.onCancel} />
           </header>
           <section className="modal-card-body">
             <div className="field is-horizontal">
@@ -99,9 +103,9 @@ class ExpenseEditor extends React.Component {
           </section>
           <footer className="modal-card-foot">
             <button className={classNames("button", "is-primary", {"is-loading": this.state.submitting})}
-                    onClick={() => this.saveChanges()}>Save changes</button>
+                    onClick={this._saveChanges}>Save changes</button>
             <button className="button"
-                    onClick={() => this.props.onClose()}>Cancel</button>
+                    onClick={this.props.onEditCanceled}>Cancel</button>
           </footer>
         </div>
       </div>
@@ -113,18 +117,20 @@ class ExpenseEditor extends React.Component {
 class ExpenseDeleter extends React.Component {
   static propTypes = {
     expense: PropTypes.object.isRequired,
-    onClose: PropTypes.func.isRequired,
+    onDeleteConfirmed: PropTypes.func.isRequired,
+    onDeleteCanceled: PropTypes.func.isRequired,
   };
 
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props);
 
     this.state = {
       submitting: false,
     }
   }
 
-  deleteExpense = () => {
+  _handleDeletePressed = (e) => {
+    e.preventDefault();
     this.setState({
       submitting: true
     });
@@ -134,34 +140,30 @@ class ExpenseDeleter extends React.Component {
       credentials: 'include',
       method: "DELETE",
     }).then((r) => {
-      this.props.onClose(true);
+      this.props.onDeleteConfirmed(this.props.expense);
     });
-
   };
 
-  render = () => {
-    return (
-      <div className="modal is-active">
-        <div className="modal-background"/>
-        <div className="modal-card">
-          <header className="modal-card-head">
-            <p className="modal-card-title">Delete Expense</p>
-            <button className="delete" onClick={() => this.props.onClose(false)} />
-          </header>
-          <section className="modal-card-body">
-            <p>Are you sure you want to delete <b>{this.props.expense.notes}</b>?</p>
-          </section>
-          <footer className="modal-card-foot">
-            <button className={classNames("button", "is-danger", {"is-loading": this.state.submitting})}
-                    onClick={() => this.deleteExpense()}>Delete</button>
-            <button className="button"
-                    onClick={() => this.props.onClose(false)}>Cancel</button>
-          </footer>
-        </div>
+  render = () => (
+    <div className="modal is-active">
+      <div className="modal-background"/>
+      <div className="modal-card">
+        <header className="modal-card-head">
+          <p className="modal-card-title">Delete Expense</p>
+          <button className="delete" onClick={this.props.onCancel} />
+        </header>
+        <section className="modal-card-body">
+          <p>Are you sure you want to delete <b>{this.props.expense.notes}</b>?</p>
+        </section>
+        <footer className="modal-card-foot">
+          <button className={classNames("button", "is-danger", {"is-loading": this.state.submitting})}
+                  onClick={this._handleDeletePressed}>Delete</button>
+          <button className="button"
+                  onClick={this.props.onDeleteCanceled}>Cancel</button>
+        </footer>
       </div>
-    );
-  }
-
+    </div>
+  );
 }
 
 class ExpenseForm extends React.Component {
@@ -170,7 +172,7 @@ class ExpenseForm extends React.Component {
     onSave: PropTypes.func.isRequired
   };
 
-  constructor(props, context) {
+  constructor(props) {
     super();
 
     this.state = {
@@ -212,7 +214,7 @@ class ExpenseForm extends React.Component {
     );
   };
 
-  saveExpense = () => {
+  saveExpense = async () => {
     const formData = new FormData();
     formData.append("amount", this.state.amount);
     formData.append("notes", this.state.notes);
@@ -221,41 +223,124 @@ class ExpenseForm extends React.Component {
       submitting: true
     });
 
-    fetch("/api/expenses", {
+    const response = await fetch("/api/expenses", {
       credentials: 'same-origin',
       method: "put",
       body: formData
-    }).then((r) => {
-      this.setState({
+    });
+    const newExpense = (await response.json()).data;
+    this.setState({
         amount: null,
         notes: "",
         submitting: false,
-      });
-      this.props.onSave();
-    })
+    });
+    this.props.onSave(newExpense);
   }
 
 }
 
+
+class NotificationBanner extends React.Component {
+
+  static propTypes = {
+    remainder: PropTypes.number.isRequired,
+  };
+
+  _getNotificationMessage = (remainder) => {
+    if (remainder < 0) {
+      return `You're over the budget by $${Number(-remainder / 100).toFixed(2)}`;
+    } else if (remainder === 0) {
+      return `You're right on budget!`;
+    } else {
+      return `You have $${Number(remainder / 100).toFixed(2)} left to spend`;
+    }
+  };
+
+  render = () => (
+    <div className={classNames({
+      'notification': true,
+      'has-text-centered': true,
+      'is-danger': this.props.remainder < 0,
+      'is-info': this.props.remainder > 0,
+    })}>
+      {this._getNotificationMessage(this.props.remainder)}
+    </div>
+  );
+
+}
+
+const ExpenseTable = ({expenses, onSelect, onDeletePressed}) => (
+  <table className="table is-fullwidth is-striped is-hoverable">
+    <thead>
+    <tr>
+      <th>Amount</th>
+      <th>Date</th>
+      <th>Notes</th>
+      <th />
+    </tr>
+    </thead>
+    <tbody>
+    {expenses
+      .slice()
+      .sort((a, b) => moment(b.timestamp).valueOf() - moment(a.timestamp).valueOf())
+      .map(expense => (
+      <tr key={expense.id} onClick={e => {
+        e.preventDefault();
+        onSelect(expense);
+      }}>
+        <td>${Number(expense.amount / 100).toFixed(2)}</td>
+        <td>{moment(expense.timestamp).format("lll")}</td>
+        <td>{expense.notes}</td>
+        <td>
+          <button className="delete"
+                  onClick={e => {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    onDeletePressed(expense);
+                  }} />
+        </td>
+      </tr>
+    ))}
+    </tbody>
+  </table>
+);
+
+
 class BudgetPage extends React.Component {
 
-  constructor(props, context) {
-    super(props, context);
+  constructor(props) {
+    super(props);
     this.state = {
-      notificationDismissed: false,
-      expenses: [],
-      selectedExpense: null,
-      deletingExpense: null,
+      loading: true,
+      error: null,
+
+      budgets: null,
+      expenses: null,
+
+      expenseBeingDeleted: null,
+      expenseBeingEdited: null,
     };
 
-    this.loadExpenses();
   }
 
-  loadExpenses = async () => {
-    const [budgets, expenses] = await Promise.all([this._loadBudgets(), this._loadExpenses()]);
-    this.setState({
-      budgets, expenses
-    });
+  componentDidMount = () => {
+    this._loadInitialData();
+  };
+
+  _loadInitialData = async () => {
+    try {
+      const [budgets, expenses] = await Promise.all([this._loadBudgets(), this._loadExpenses()]);
+      this.setState({
+        loading: false,
+        budgets,
+        expenses
+      });
+    } catch (response) {
+      this.setState({
+        loading: false,
+        error: response.message,
+      });
+    }
   };
 
   _loadBudgets = async () => {
@@ -272,121 +357,90 @@ class BudgetPage extends React.Component {
     return (await response.json()).data;
   };
 
-  closeExpenseEditor = (changed) => {
+  _handleDeletePressed = (expense) => {
     this.setState({
-      selectedExpense: null,
+      expenseBeingDeleted: expense,
     });
-
-    if (changed) {
-      this.clearAndReloadExpenses();
-    }
   };
 
-  closeExpenseDeleter = (changed) => {
-    this.setState({
-      deletingExpense: null,
-    });
-
-    if (changed) {
-      this.clearAndReloadExpenses();
-    }
+  _handleDeleteConfirmed = (deletedExpense) => {
+    this.setState(({expenses}) => ({
+      expenses: expenses.filter(expense => expense.id !== deletedExpense.id),
+      expenseBeingDeleted: null,
+    }));
   };
 
-  clearAndReloadExpenses = () => {
+  _handleDeleteCanceled = () => {
     this.setState({
-      expenses: [],
+      expenseBeingDeleted: null,
     });
-    this.loadExpenses();
   };
 
-  _getAmountAllowed = () => {
-    return this.state.budgets
+  _handleEditPressed = (expense) => {
+    this.setState({
+      expenseBeingEdited: expense
+    });
+  };
+
+  _handleExpenseAdded = (addedExpense) => {
+    this.setState(({expenses}) => ({
+      expenses: [...expenses, addedExpense]
+    }));
+  };
+
+  _handleEditedConfirmed = (editedExpense) => {
+    this.setState(({expenses}) => ({
+      expenses: [editedExpense, ...expenses.filter(expense => expense.id !== editedExpense.id)],
+      expenseBeingEdited: null,
+    }));
+  };
+
+  _handleEditCanceled = () => {
+    this.setState({
+      expenseBeingEdited: null,
+    });
+  };
+
+  _getAmountAllowed = (budgets) => {
+    return budgets
       .map(budget => {
-        const end = moment(budget.end_timestamp).isValid() ? moment(budget.end_timestamp) : moment();
+        const end = moment(budget.end_timestamp).isValid() ? moment(budget.end_timestamp) : moment().tz('America/Los_Angeles');
         const start = moment(budget.start_timestamp);
         return budget.amount * end.diff(start, 'days');
       })
       .reduce(((a, b) => a + b), 0);
   };
 
-  render = () => {
+  _getAmountSpent = (expenses) => {
+    return expenses.map(expense => expense.amount).reduce(((a, b) => a + b), 0);
+  };
 
-    if (!this.state.budgets) {
+  render = () => {
+    if (this.state.loading) {
       return <div/>;
     }
 
-    const amountAllowed = this._getAmountAllowed();
-    const totalSpent = this.state.expenses.map((expense) => expense.amount).reduce(((a, b) => a + b), 0);
-    const remainder = amountAllowed - totalSpent;
-
-    let notification = null;
-    if (this.state.expenses.length > 0 && remainder < 0) {
-      notification = (
-        <div className="notification is-danger has-text-centered">
-          Slow down there buddy! You're over budget by ${Number(-remainder / 100).toFixed(2)}!
-        </div>
-      );
-    } else if (this.state.expenses.length > 0) {
-      notification = (
-        <div className="notification has-text-centered">
-          You have ${Number(remainder / 100).toFixed(2)} left to spend!
-        </div>
-      );
-    }
-
-    let expenseEditor = null;
-    if (this.state.selectedExpense) {
-      expenseEditor = (
-        <ExpenseEditor expense={this.state.selectedExpense}
-                       onClose={(changed) => this.closeExpenseEditor(changed)} />
-      );
-    }
-
-    let expenseDeleter = null;
-    if (this.state.deletingExpense) {
-      expenseDeleter = (
-        <ExpenseDeleter expense={this.state.deletingExpense}
-                        onClose={(changed) => this.closeExpenseDeleter(changed)} />
-      );
-    }
+    const amountAllowed = this._getAmountAllowed(this.state.budgets);
+    const totalSpent = this._getAmountSpent(this.state.expenses);
+    const budgetRemaining = amountAllowed - totalSpent;
 
     return (
       <Page title="Budget">
         <Navigation/>
-        <div className="container">
+        <div className="container section">
           <h1 className="title has-text-centered">Budget</h1>
-          {expenseEditor}
-          {expenseDeleter}
-          {notification}
-
-          <ExpenseForm onSave={() => this.clearAndReloadExpenses()}/>
-
-          <table className="table is-fullwidth is-striped is-hoverable">
-
-            <thead>
-            <tr>
-              <th>Amount</th>
-              <th>Date</th>
-              <th>Notes</th>
-              <th />
-            </tr>
-            </thead>
-            <tbody>
-            {this.state.expenses.map(expense => {
-              return (
-                <tr key={expense.id} onClick={() => this.setState({selectedExpense: expense})}>
-                  <td>${Number(expense.amount / 100).toFixed(2)}</td>
-                  <td>{moment(expense.timestamp).format("lll")}</td>
-                  <td>{expense.notes}</td>
-                  <td>
-                    <button className="delete"
-                            onClick={(e) => {e.stopPropagation(); this.setState({deletingExpense: expense})}} />
-                  </td>
-                </tr>
-              )
-            })}
-            </tbody>
-          </table>
+          {this.state.expenseBeingDeleted && <ExpenseDeleter expense={this.state.expenseBeingDeleted}
+                                                             onDeleteConfirmed={this._handleDeleteConfirmed}
+                                                             onDeleteCanceled={this._handleDeleteCanceled}/>
+          }
+          {this.state.expenseBeingEdited && <ExpenseEditor expense={this.state.expenseBeingEdited}
+                                                           onEditConfirmed={this._handleEditedConfirmed}
+                                                           onEditCanceled={this._handleEditCanceled}/>}
+          <NotificationBanner remainder={budgetRemaining}/>
+          <ExpenseForm onSave={this._handleExpenseAdded}/>
+          <ExpenseTable expenses={this.state.expenses}
+                        onSelect={this._handleEditPressed}
+                        onDeletePressed={this._handleDeletePressed}/>
         </div>
       </Page>
     );
